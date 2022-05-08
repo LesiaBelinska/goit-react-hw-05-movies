6b0627fed863fd35adae6e87ab09b328d2db02d3
LesiaBelinska <lm.kudak@gmail.com>
2022-05-09 00:03:46 +0300

    Create AppBar.jsx

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,9 @@
+import { Navigation } from "components/Navigation/Navigation.jsx";
+
+export const AppBar = () => {
+    return (
+        <header>
+            <Navigation />
+        </header>
+    )
+}
