9e24bfbc6eb0948fddfb4dc88ec34484fd5418ce
LesiaBelinska <lm.kudak@gmail.com>
2022-05-09 00:05:20 +0300

    add Routes, Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./Layout/Layout.jsx";
+import { HomePage } from "pages/HomePage/HomePage.jsx";
+import { MoviesPage } from "pages/MoviesPage/MoviesPage.jsx";
+import { MovieDetailsPage } from "pages/MovieDetailsPage/MovieDetailsPage.jsx";
+import { Cast } from "pages/Cast/Cast.jsx";
+import { Reviews } from "pages/Reviews/Reviews.jsx";
+import { NotFoundPage } from "pages/NotFoundPage/NotFoundPage.jsx";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" exact element={<MoviesPage />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<Cast />}/>
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+
+    
   );
 };
