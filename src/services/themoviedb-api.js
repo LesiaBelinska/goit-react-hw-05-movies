75670c73fe6c301ea7b7805d15fae1a3d893aa70
LesiaBelinska <lm.kudak@gmail.com>
2022-05-10 12:05:11 +0300

    Update themoviedb-api.js

--- src/services/themoviedb-api.js
+++ src/services/themoviedb-api.js
@@ -3,37 +3,38 @@ const API_KEY = '1a4768bd62313a9ba272b58353515c76';
 
 export const POSTER_URL = 'https://image.tmdb.org/t/p/w500';
 
-async function fetchWithErrorHandling(url = '', config = {}) {
-    const response = await fetch(url, config);
-    return response.ok
-        ? await response.json()
-        : Promise.reject(new Error('Not found'));
-};
 
 export function fetchTrendingMovies() {
-    return fetchWithErrorHandling(
-        `${BASE_URL}trending/movie/day?api_key=${API_KEY}`);
+    return fetch(
+        `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
+    )
+        .then(response => response.json());
 };
 
 export function fetchSearchMovies(query) {
-    return fetchWithErrorHandling(
-        `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`)
+    return fetch(
+        `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+    )
+        .then(response => response.json());
 };
 
 export function fetchMovieDetails(movieId) {
-    return fetchWithErrorHandling(
+    return fetch(
         `${BASE_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
     )
+        .then(response => response.json());
 };
 
 export function fetchMovieCast(movieId) {
-    return fetchWithErrorHandling(
+    return fetch(
         `${BASE_URL}movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
     )
+        .then(response => response.json());
 };
 
 export function fetchMovieReviews(movieId) {
-    return fetchWithErrorHandling(
+    return fetch(
         `${BASE_URL}movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
     )
+        .then(response => response.json());
 };
