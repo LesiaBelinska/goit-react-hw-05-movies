895212a4222e0114426eaeb565915ff4b2154b74
LesiaBelinska <lm.kudak@gmail.com>
2022-05-11 13:14:02 +0300

    update GoBack

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -24,38 +24,38 @@ export default function MovieDetailsPage() {
     
     return (
         <>
-            <BackLink onClick={onGoBack} label="Go back"/>
+            <BackLink onClick={onGoBack} label="Go back" />
             {movie &&
                 <>
-                <img
-                    src={api.POSTER_URL + movie.poster_path}
-                    alt={movie.title}
-                    width="300"
-                    height="450"
-                />
-                <h2>{movie.title}</h2>
-                <p>User Score: <span>{movie.vote_average}</span></p>
-                <h3>Overview</h3>
-                <p>{movie.overview}</p>
-                {movie.genres && (
-                    <>
-                        <p>Genres</p>
-                        <ul>
-                            {movie.genres.map((genre, index) =>
-                                <li key={index}>{genre.name}</li>)}
-                        </ul>
-                    </>
-                )}
-                <p>Additional information</p>
-                <nav>
-                    <NavLink to={'cast'}>Cast</NavLink>
-                    <NavLink to={'reviews'}>Reviews</NavLink>
-                </nav>
-            </>
+                    <img
+                        src={api.POSTER_URL + movie.poster_path}
+                        alt={movie.title}
+                        width="300"
+                        height="450"
+                    />
+                    <h2>{movie.title}</h2>
+                    <p>User Score: <span>{movie.vote_average}</span></p>
+                    <h3>Overview</h3>
+                    <p>{movie.overview}</p>
+                    {movie.genres && (
+                        <>
+                            <p>Genres</p>
+                            <ul>
+                                {movie.genres.map((genre, index) =>
+                                    <li key={index}>{genre.name}</li>)}
+                            </ul>
+                        </>
+                    )}
+                    <p>Additional information</p>
+                    <nav>
+                        <NavLink to={'cast'} state={location.state}>Cast</NavLink>
+                        <NavLink to={'reviews'} state={location.state}>Reviews</NavLink>
+                    </nav>
+                </>
                 
             
             }
-            <Outlet/>
+            <Outlet />
         </>
     )
 }
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,51 +1,61 @@
 import { useState, useEffect } from "react";
 import { Link, useSearchParams, useLocation } from "react-router-dom";
+import { toast } from "react-hot-toast";
 import { SearchForm } from "components/SearchForm/SearchForm.jsx";
-import * as api from "services/themoviedb-api.js";
+import * as API from "services/themoviedb-api.js";
 
 
 
 export default function MoviesPage() {
-
-    const [searchQuery, setSearchQuery] = useState('');
+ 
+    const [searchQuery, setSearchQuery] = useState("");
     const [movies, setMovies] = useState([]);
-    const [, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
     const location = useLocation();
-
+    const currentSearchParams = searchParams.get('query');
+    
     
     useEffect(() => {
         if (searchQuery === '') {
             return;
         }
 
-        api.fetchSearchMovies(searchQuery)
+        API.fetchSearchMovies(searchQuery)
             .then(response => {
-                setMovies(response.results);
+                if (!response.results.length) {
+                    toast.error(`No information about "${searchQuery}"`)
+                }
+                setMovies(response.results)
             });
     }, [searchQuery]);
 
+
+    useEffect(() => {
+    if (currentSearchParams) {
+      setSearchQuery(currentSearchParams);
+    }
+    }, [currentSearchParams]);
+    
 
     const onFormSubmit = (value) => {
         setSearchQuery(value);
         setMovies([]);
         setSearchParams({ query: value });
-    }
+    };
+
 
     return (
         <>
             <SearchForm onSubmit={onFormSubmit} />
-            
-                <ul>
-                    {movies && movies.map(movie => 
-                        <li key={movie.id}>
-                            <Link to={`/movies/${movie.id}`} state={{from: location}}>{movie.title}</Link>
-                        </li>
-               )}
+            <ul>
+                {movies && movies.map(movie =>
+                    <li key={movie.id}>
+                        <Link to={`/movies/${movie.id}`} state={{ from: location }}>{movie.title}</Link>
+                    </li>
+                )}
             </ul>
             
         </>
-        
-        
     )
 }
 
