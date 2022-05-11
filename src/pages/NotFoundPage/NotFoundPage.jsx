830f6d0abd0ac41cca7f8f32290ad6f2c4782213
LesiaBelinska <lm.kudak@gmail.com>
2022-05-11 22:22:34 +0300

    add NotFoundPage's styles

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,7 @@
+import s from "./NotFoundPage.module.css";
 
 export default function NotFoundPage() {
     return (
-        <h1>404 PAGE NOT FOUND</h1>
+        <h1 className={s.title}>404 PAGE NOT FOUND</h1>
     )
 };
